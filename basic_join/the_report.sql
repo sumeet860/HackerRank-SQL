889d2798eae3ecc82054c780b761213c19795536
Sumeet Kumar Singh <41811254+sumeet860@users.noreply.github.com>
2022-10-23 19:24:50 +0530

    Create the_report.sql

---
+++ basic_join/the_report.sql
@@ -0,0 +1,8 @@
+select 
+    case when g.grade < 8 then 'NULL'
+    else s.name end,
+    g.grade, s.marks
+from
+    students s
+join grades g on s.marks between g.min_mark and g.max_mark
+order by g.grade desc, s.name,s.marks asc
